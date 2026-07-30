d4e89f89448d6db9d02f1ff79bc1554a1eff2c5e
Anuva Agarwal <anuva.agrawal1906@gmail.com>
2026-07-30 00:21:03 -0700

    feature: user login with password added

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -1,4 +1,21 @@
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta
+import jwt
+import os
+from sqlmodel import Session, select
+
+# from backend.crud.users import get_user_by_email
+from backend.models.users import UserInDB
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+authentication_scheme = OAuth2PasswordBearer(tokenUrl="/api/token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +26,24 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    data_to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    data_to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(data_to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def authenticate_user(session: Session, email: str, password: str):
+    user = session.exec(select(UserInDB).where(UserInDB.email == email)).first()
+    # user = get_user_by_email(session, email)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
