23078a7965774395ba5971fc501198dde68f2d9d
Anuva Agarwal <anuva.agrawal1906@gmail.com>
2026-07-30 00:21:03 -0700

    feature: user login with password added

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,18 +1,20 @@
 from fastapi import APIRouter, HTTPException, status, BackgroundTasks, Depends
+from typing import Annotated
 from sqlmodel import Session
 from backend.crud.users import get_user_by_email, create_user
 from backend.crud.database import get_session
 from backend.schemas.users import UserCreate, UserRead
 from backend.utils.email import send_email_async
-
+from backend.models.auth import Token
+from fastapi.security import OAuth2PasswordRequestForm
+from backend.utils.security import authenticate_user
+from datetime import timedelta
+from backend.utils.security import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
 
 router = APIRouter(prefix="/api")
 
 
-@router.post(
-    "/register",
-    response_model=UserRead,
-)
+@router.post("/register", response_model=UserRead)
 async def register(
     background_tasks: BackgroundTasks,
     user_in: UserCreate,
@@ -22,7 +24,8 @@ async def register(
     user = get_user_by_email(session, user_in.email)
     if user:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered"
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered",
         )
 
     # save user in database
@@ -36,3 +39,35 @@ async def register(
 
     # user = UserRead(id=1, email=user_in.email)
     return user
+
+
+@router.post("/token")
+async def login(
+    form_data: Annotated[
+        OAuth2PasswordRequestForm,
+        Depends(),
+    ],
+    session: Session = Depends(get_session),
+) -> Token:
+    user = authenticate_user(session, form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Email already registered",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+# def authenticate_user(session: Session, email: str, password: str):
+#     user = get_user_by_email(session, email)
+#     if not user:
+#         return False
+#     if not verify_password(password, user.password):
+#         return False
+#     return user
